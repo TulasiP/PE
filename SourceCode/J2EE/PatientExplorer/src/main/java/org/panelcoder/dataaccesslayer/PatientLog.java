24bb7c126e4b39e70a9599af0fc56df19424ab4c
TulasiP <meghana.tulasi@yahoo.com>
2017-01-26 11:14:45 +0530

    Aut generate the uid value

--- SourceCode/J2EE/PatientExplorer/src/main/java/org/panelcoder/dataaccesslayer/PatientLog.java
+++ SourceCode/J2EE/PatientExplorer/src/main/java/org/panelcoder/dataaccesslayer/PatientLog.java
@@ -1,12 +1,14 @@
 package org.panelcoder.dataaccesslayer;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class PatientLog 
 {
-	@Id
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	private long UUID;
 	private String firstname;
 	private String lastname;
